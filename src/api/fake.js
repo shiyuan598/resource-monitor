0e6d9ed6f62a47d87860aa05b209026131f759d2
王士远 <wangshiyuan@192.168.2.103>
2023-07-25 07:53:30 +0800

    fixable

--- src/api/fake.js
+++ src/api/fake.js
@@ -90,6 +90,7 @@ const getData = () => {
     }));
 }
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
     getData: getData
 };
--- src/views/main/index.tsx
+++ src/views/main/index.tsx
@@ -2,30 +2,31 @@ import React, { useState, useEffect } from "react";
 import fakeData from "../../api/fake";
 import {formatBytes} from "../../utils/tools";
 import { Progress } from "antd";
-import { doc } from "prettier";
 
 export default function App() {
    const [diskList, setDiskList] = useState<any>([]);
     useEffect(() => {
-        setInterval(()=>{
-            setDiskList(fakeData.getData().slice(0, 30));
+        setInterval(() => {
+            setDiskList(fakeData.getData());
         }, 10000);
+        
     }, []);
     return (
-        <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 gap-4 p-4">
+        <div className="grid grid-cols-3 sm:grid-cols-3 text-xs sm:text-xs md:grid-cols-5 md:text-small lg:grid-cols-8 lg:text-base sm:gap-2 md:gap-4 lg:gap-6 gap-4 p-4">
             {diskList.map((item: any) => (
                 <>
-                    <div key={item.id} className="border border-gray-800 text-center relative">
+                    <div key={item.id} className="border border-gray-800 text-center text-gray-300 relative">
                         <div className="absolute left-0 bg-emerald-700 px-1">{item.location}</div>
                         <div className="mt-6">
                             <Progress
+                            className="custom-progress"
                                 type="circle"
                                 size={"small"}
                                 percent={item.progress}
                             />
                         </div>
                         <span className="block mt-2 flex-nowrap whitespace-nowrap">{formatBytes(item.total * item.progress / 100)} / {formatBytes(item.total)}</span>
-                        <span className="block font-bold text-lg mb-2">{item.name}</span>
+                        <span className="block font-bold text-base sm:text-base md:text-lg lg:text-xl mb-2">{item.name}</span>
                     </div>
                 </>
             ))}
